b2903b13573a2b5d73f27cab0884006a46b6b5d9
Luk1Pr0 <lpytel16@gmail.com>
2020-11-19 13:04:23 +0100

    Create all HTML elements with JS and populate them with data from the API

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const resultsNav = document.getElementById("resultsNav");
+const favouritesNav = document.getElementById("favouritesNav");
+const imagesContainer = document.querySelector(".images-container");
+const saveConfirmed = document.querySelector(".save-confirmed");
+const loader = document.querySelector(".lodaer");
 
 // NASA API
 const count = 5;
@@ -6,6 +11,55 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 
 let resultsArray = [];
 
+function updateDom() {
+    resultsArray.map(result => {
+        // Create card container
+        const card = document.createElement("div");
+        card.classList.add("card");
+        // Create link element
+        const link = document.createElement("a");
+        link.href = result.hdurl;
+        link.title = "View full image";
+        link.target = "_blank";
+        // Create image
+        const image = document.createElement("img");
+        image.src = result.url;
+        image.alt = "NASA picture of the day";
+        image.loading = "lazy";
+        image.classList.add("card-img-top");
+        // Card body
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        // Card title
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.textContent = result.title;
+        // Save text
+        const saveText = document.createElement("p");
+        saveText.classList.add("clickable");
+        saveText.textContent = "Add to favourites";
+        // Card text
+        const cardText = document.createElement("p");
+        cardText.textContent = result.explanation;
+        // Footer container
+        const footer = document.createElement("small");
+        footer.classList.add("text-muted");
+        // Date
+        const date = document.createElement("strong");
+        date.textContent = result.date;
+        // Copyright
+        const copyrightResult = result.copyright === undefined ? "" : result.copyright;
+        const copyright = document.createElement("span");
+        copyright.textContent = ` ${copyrightResult} `;
+        // Append elements
+        footer.append(copyright, date);
+        cardBody.append(cardTitle, saveText, cardText, footer);
+        link.appendChild(image);
+        card.append(link, cardBody, footer);
+        imagesContainer.appendChild(card);
+    })
+}
+
 // Get 5 images from the API
 function getData() {
     fetch(apiUrl)
@@ -13,6 +67,7 @@ function getData() {
         .then(data => {
             resultsArray = data;
             console.log(resultsArray);
+            updateDom();
         })
     .catch(err => console.log(err));
 }
